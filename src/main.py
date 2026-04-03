d07cbdc3b5723c5a113071929c9798da8f3f2fde
Leo Timmins <leotimmins1974@gmail.com>
2026-04-03 19:54:50 +0800

    working point rendering

--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
+import pygame
+import numpy as np
+
 import assets.obj as obj
 
 import render.math as math
@@ -6,21 +9,56 @@ import render.graphics as graphics
 import tools.config as config
 import tools.results as results
 
-
 MESHES_FOLDER_PATH = "./meshes/"
 SIMULATION_CONFIG_PATH = "./simulation.config"
-
-# Grab configs
-config = config.parse_config(SIMULATION_CONFIG_PATH)
-
-# TEST CUBE
-cube_mesh = obj.load(MESHES_FOLDER_PATH + "cube.obj")
+TARGET_FPS = 60
+BACKGROUND_RGB = (15,15,30)
 
 # ENTRY POINT
 def main() -> exit_code:
 
+    # Grab configs
+    configuration = config.parse_config(SIMULATION_CONFIG_PATH)
 
+    # TEST CUBE
+    cube_mesh = obj.load(MESHES_FOLDER_PATH + "cube.obj")
+    cube_mesh.transform.translate[0] = -3
 
+    camera = graphics.Camera()
+
+    render_order = [cube_mesh]
+
+    # Window/Buffer manager 
+    pygame.init()
+    pygame.display.set_caption("3D Peep Simulation")
+
+    window = pygame.display.set_mode((graphics.WIDTH, graphics.HEIGHT))
+
+    close = False
+    while not close:
+        # EVENT LOOP
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                close = True
+
+        window.fill(BACKGROUND_RGB)
+        # SIM LOGIC
+        cube_mesh.transform.rotate[0] += 0.01
+        cube_mesh.transform.rotate[1] += 0.01
+        cube_mesh.transform.rotate[2] += 0.01
+        cube_mesh.transform.update_model()
+        #cube_mesh.transform.translate[0] -= 1
+        # DRAW
+        for mesh in render_order:
+            for v in mesh.verticies:
+                clip_coordinates = camera.projection @ camera.view @ mesh.transform.model @ np.array([v.pos[0], v.pos[1], v.pos[2], 1.])
+                if clip_coordinates[3] != 0:
+                    window.set_at(graphics.to_screen_space(clip_coordinates, camera.resolution[0], camera.resolution[1]), (255,255,255))
+
+        pygame.time.delay(TARGET_FPS)
+        pygame.display.update()
+
+    pygame.quit()
     return(0)
 
 
--- src/render/graphics.py
+++ src/render/graphics.py
@@ -1,18 +1,60 @@
 # PYGAME OR SDL2 (ILL CHOOSE LATER) HELPER FUNCTIONS AND STRUCTS
 
+import numpy as np
+import math as mth
+
 WIDTH,HEIGHT = (600,500)
 
 class Transform:
-    translate = [0.,0.,0.]
-    rotate = [0.,0.,0.] # Radians
-    scale = [1.,1.,1.] 
+    def __init__(self):
+        self.translate = [0.,0.,0.]
+        self.rotate = [0.,0.,0.] # Radians
+        self.scale = [1.,1.,1.] 
+        self.update_model()
+
+    def update_model(self):
+        scale = np.array([
+            [self.scale[0],0.,0.,0.],
+            [0.,self.scale[1],0.,0.],
+            [0.,0.,self.scale[2],0.],
+            [0.,0.,0.,1.],
+        ])
+
+        rotate_x = np.array([
+            [1.,0.,0.,0.],
+            [0., mth.cos(self.rotate[0]), -mth.sin(self.rotate[0]), 0.],
+            [0., mth.sin(self.rotate[0]), mth.cos(self.rotate[0]), 0.],
+            [0.,0.,0.,1.]
+        ])
+
+        rotate_y = np.array([
+            [mth.cos(self.rotate[1]), 0., mth.sin(self.rotate[1]), 0.],
+            [0., 1., 0., 0.],
+            [-mth.sin(self.rotate[1]), 0., mth.cos(self.rotate[1]), 0.],
+            [0., 0., 0., 1.]
+        ])
+
+        rotate_z = np.array([
+            [mth.cos(self.rotate[2]), -mth.sin(self.rotate[2]), 0., 0.],
+            [mth.sin(self.rotate[2]),  mth.cos(self.rotate[2]), 0., 0.],
+            [0., 0., 1., 0.],
+            [0., 0., 0., 1.]
+        ])
+
+        translate = np.array([
+            [1., 0., 0., self.translate[0]],
+            [0., 1., 0., self.translate[1]],
+            [0., 0., 1., self.translate[2]],
+            [0., 0., 0., 1.]
+        ])
+
+        self.model = translate @ rotate_z @ rotate_y @ rotate_x @ scale
 
 
 class Mesh:
-    transform = Transform()
-    color = [200,200,200,255] # RGBA | Defaults to grey
-
     def __init__(self, verticies: [Vertex], faces: [Face]):
+        self.transform = Transform()
+        self.color = [200,200,200,255] # RGBA | Defaults to grey
         self.verticies = verticies
         self.faces = faces
 
@@ -26,13 +68,35 @@ class Vertex:
         self.pos = pos
 
 class Camera:
-    transform = Transform()
-    fov = 1.6 # Radians
-    up = [0.,1.,0.]
-    forward = [-1.,0.,0.]
-    right = [0.,0.,-1.]
-    resolution = [WIDTH,HEIGHT]
-    aspect = float(WIDTH) / float(HEIGHT)
-    near = 0.1
-    far = 1000. # Might lower to improve preformance
+    def __init__(self):
+        self.transform = Transform()
+        self.fov = 1.6 # Radians
+        self.up = [0.,1.,0.]
+        self.forward = [-1.,0.,0.]
+        self.right = [0.,0.,-1.]
+        self.resolution = [WIDTH,HEIGHT]
+        self.aspect = float(WIDTH) / float(HEIGHT)
+        self.near = 0.1
+        self.far = 1000. # Might lower to improve preformance
+        self.update_projection()
+        self.update_view()
 
+    def update_projection(self):
+        t = mth.tan(self.fov/2)
+        self.projection = np.array([
+            [(1./(self.aspect * t)), 0., 0., 0.],
+            [0., 1./t, 0.,0.],
+            [0.,0.,-(self.far + self.near) / (self.far - self.near), -2. * self.far * self.near / (self.far - self.near)],
+            [0.,0.,-1.,0.]
+        ])
+
+    def update_view(self):
+        self.view = np.array([
+            [self.right[0], self.right[1], self.right[2], -np.dot(self.right, self.transform.translate)],
+            [self.up[0], self.up[1], self.up[2], -np.dot(self.up, self.transform.translate)],
+            [self.forward[0], self.forward[1], self.forward[2], -np.dot(self.forward, self.transform.translate)],
+            [0.,0.,0.,1.]
+        ])
+
+def to_screen_space(clip_coord, width, height):
+    return((int(round((clip_coord[0]/clip_coord[3]+1) / 2. * width,0)), int(round((1.-clip_coord[1] / clip_coord[3])/2.*height,0)) ))
